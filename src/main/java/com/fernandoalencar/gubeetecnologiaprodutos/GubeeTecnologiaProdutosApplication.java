d381e9a4b23b5379151c722d31d2f10d65b4cf6a
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-22 23:36:12 -0300

    first commit

---
+++ src/main/java/com/fernandoalencar/gubeetecnologiaprodutos/GubeeTecnologiaProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.fernandoalencar.gubeetecnologiaprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GubeeTecnologiaProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GubeeTecnologiaProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandoalencar/gubeetecnologiaprodutos/GubeeTecnologiaProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandoalencar.gubeetecnologiaprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GubeeTecnologiaProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
