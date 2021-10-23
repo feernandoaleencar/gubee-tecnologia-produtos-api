96dfd21051704d65f49077fbc9f9962a9a7ade9e
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-23 00:23:07 -0300

    criando controller da entidade produtos

---
+++ src/main/java/com/fernandoalencar/gubeetecnologiaprodutos/api/controller/ProdutoController.java
@@ -0,0 +1,10 @@
+package com.fernandoalencar.gubeetecnologiaprodutos.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+}
